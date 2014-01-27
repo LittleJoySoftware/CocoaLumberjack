87d041ccd58da0d46dc2d3f39f4bce9e6b78e428
Joshua Moody <joshuajmoody@gmail.com>
2014-01-27 19:12:51 +0100

    no longer loading cocoalumberjack extensions

--- Lumberjack/LJS/Lumberjack.h
+++ Lumberjack/LJS/Lumberjack.h
@@ -30,7 +30,5 @@
 #import "LjsDefaultLogFormatter.h"
 #import "LjsLogFileManager.h"
 #import "DDFileLogger+CurrentFile.h"
-#import "ContextFilterLogFormatter.h"
-#import "DispatchQueueLogFormatter.h"
 #import "LjsLogging.h"
 
